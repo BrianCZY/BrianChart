af0312560fc87199aa391a6933cfbe236ed6fef4
Brian <czy@huxijia.com>
2026-05-20 16:07:32 +0800

    合并master

--- BrianChart/src/main/java/com/brian/chart/compose/view/chart/PointData.kt
+++ BrianChart/src/main/java/com/brian/chart/compose/view/chart/PointData.kt
@@ -1,6 +1,6 @@
 package com.brian.chart.compose.view.chart
 
-data class PointData(
+data class AxisPoints(
     val point0: Point = Point(),
     val point1: Point = Point(),
     val point2: Point = Point(),
